ecb2a7ee28fb6c251f8db47c08e0fac46af7c724
prid <prid.314@gmail.com>
2015-10-23 14:02:10 -0400

    Create data.js

---
+++ data.js
@@ -0,0 +1,23 @@
+events=[
+        {"name": "Student Recital",
+            "location": "Paul Hall",
+			"date": "2015-10-30",
+			"time": "14:10",
+			"categories":["student event","concerts"],
+			"description":"Come to the student recital and support Juilliard performers!"
+			},
+        {"name": "Pre-college reunion",
+            "location": "Morse Hall",
+			"date": "2015-11-05",
+			"time": "17:00",
+			"categories":["alumini event","pre-college"],
+			"description":"Pre college reunion!"
+			},
+		{"name": "Faculty Recital",
+            "location": "Peter Jay Sharp Hall",
+			"date": "2015-9-30",
+			"time": "14:0",
+			"categories":["faculty event","concerts"],
+			"description":"Come to the faculty recital and support Juilliard performers!"
+			}
+    ]
